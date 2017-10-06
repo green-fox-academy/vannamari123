020ef655757f2fea9a24d29c1ba53f1d0287b100
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 13:18:55 +0200

    Solves task 13 of day 4

---
+++ week-01/day-04/13.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    double f;
+    printf("Fahrenheit\t\t   Celsius\n");
+    for (f=0; f<200; f+=20) {
+        printf("%.f\t\t\t\t\t%.1f \n", f, ((f-32)*5/9));
+    }
+    
+    return 0;
+}
+
+

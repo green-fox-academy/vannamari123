fbf18e534dceea0a30d449b1cff28e81fc32b818
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 11:43:43 +0200

    Solves task 02 of day 4

---
+++ week-01/day-04/02.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int a = -212;
+    if (a >= 0)
+        printf("The number is positive. \n");
+    else
+        printf("The number is negative. \n");
+
+return 0;
+}
+

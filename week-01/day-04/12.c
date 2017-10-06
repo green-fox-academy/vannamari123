5a93ee7c51cc0626a6241b0b9eaeb5bdcd16b68d
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 13:07:39 +0200

    Solves task 12 of day  with switch case

--- week-01/day-04/12.c
+++ week-01/day-04/12.c
@@ -24,3 +24,35 @@ int main()
     return 0;
 }
 
+
+/*int main()
+{
+    for (int i=0; i<7; i++){
+        switch (i) {
+            case 0:
+                printf("Monday \n");
+                break;
+            case 1:
+                printf("Tuesday \n");
+                break;
+            case 2:
+                printf("Wednesday \n");
+                break;
+            case 3:
+                printf("Thursday \n");
+                break;
+            case 4:
+                printf("Friday \n");
+                break;
+            case 5:
+                printf("Saturday \n");
+                break;
+            case 6:
+                printf("Sunday \n");
+                break;
+            default:
+                break;
+        }
+    }
+}
+*/

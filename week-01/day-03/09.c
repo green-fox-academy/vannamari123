ce70530b9b1dbe68fa09b6c0266ae05271fb7b45
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 10:33:12 +0200

    Solves task 09

---
+++ week-01/day-03/09.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//09.c
+int main() {
+    int i = 53625;
+    int d = i % 11;
+    if (d == 0){
+       printf("11 is a divisor.");
+    } else {
+       printf("11 is not a divisor.");
+    }
+ return 0;
+ }
+ 

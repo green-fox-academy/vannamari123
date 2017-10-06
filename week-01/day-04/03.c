a2040d8ca4839bf0324b73b5d07597257cd688d2
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 11:45:18 +0200

    Solves task 03 of day 4

---
+++ week-01/day-04/03.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    uint8_t ad = 6;
+    uint8_t m=0;
+    do {
+        m++;
+        printf("%d \n", m);
+    } while (m<ad);
+    
+return 0;
+}
+

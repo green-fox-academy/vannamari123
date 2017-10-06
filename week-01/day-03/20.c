2a8a79c5463412d47852b2344b305e728db9b267
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 11:18:02 +0200

    Solves task 20

---
+++ week-01/day-03/20.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//20.c
+
+int main()
+{
+    uint8_t z = 12;
+    if(z < 10){
+        printf("More!");
+    } else if(z < 20){
+        printf("Sweet!");
+    } else {
+        printf("Less!");
+    }
+return 0;
+}

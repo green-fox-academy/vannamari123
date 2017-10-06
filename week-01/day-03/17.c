c521c612a60e03b834aef7b9295874bb6f06b574
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 11:08:05 +0200

    Solves task 17

---
+++ week-01/day-03/17.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//17.c
+
+int main()
+{
+    int w = 24;
+    int out = 0;
+    if (w % 2 == 0){
+        ++out;
+        printf("%d \n", out);
+    }
+return 0;
+ }
+

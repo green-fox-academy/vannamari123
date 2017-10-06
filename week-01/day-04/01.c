cf1ea483e312b87d69acbd5adf351fcb79ddd657
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 11:40:53 +0200

    Solves task 01 of day 4

---
+++ week-01/day-04/01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//01.c
+
+int main()
+{
+    int age = 23;
+    float height_meter = 1.85;
+    printf("Age: %d years \nHeight: %.2f meter \n", age, height_meter);
+return 0;
+}

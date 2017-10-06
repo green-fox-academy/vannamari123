061d746b8019da30c67bcf3d29a46d309382a5a8
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 12:50:22 +0200

    Solves task 07 of day 4

---
+++ week-01/day-04/07.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int num = 14;
+    int r = 0;
+    r=num % 2;
+    switch (r) {
+        case 0:
+            printf("The number is even. \n");
+            break;
+            
+        default:
+            printf("The number is odd. \n");
+            break;
+    }
+}

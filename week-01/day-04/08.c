00f749077106d44d0fd45fedc0f72be48f515fff
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 12:52:07 +0200

    Solves task 08 of day 4

---
+++ week-01/day-04/08.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    int num = 0;
+    int r = 0;
+    while (num < 100){
+        r = num % 2;
+    switch(r){
+            case 0:
+            printf("Even \n");
+            break;
+            
+            default:
+            printf("Odd \n");
+            break;
+        
+    }
+        num++;
+    }
+return 0;
+}
+

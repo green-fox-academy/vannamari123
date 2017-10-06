92ed24d6cd12e5d8c041b47e2f68264478246320
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 11:46:56 +0200

    Solves task 04 of day 4

---
+++ week-01/day-04/04.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//04.c
+
+int main()
+{
+    uint8_t ae = 4;
+    uint8_t i=0;
+    do {
+        i++;
+        printf("yeeey \n");
+    } while(i<ae);
+return 0;
+}
+
+

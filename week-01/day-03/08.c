80be79f69ddca9e1f2281367f647244a54904809
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 10:28:45 +0200

    Solves task 08

---
+++ week-01/day-03/08.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+//08.c
+int main() {
+    int h1 = 350;
+    int h2 = 200;
+    int h3=h2*2;
+ if (h3>h1){
+    printf("A h3 nagyobb, mint h1.");
+ } else{
+    printf("A h3 kisebb, mint h1.");
+ }
+ return 0;
+ }
+ 

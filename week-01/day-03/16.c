716e658f5fe7a9d9ecfb6a1184cb143e74fd9dca
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-06 11:05:45 +0200

    Solves task 16

---
+++ week-01/day-03/16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+
+//16.c
+
+int main()
+{
+    uint16_t v = 426;
+    uint64_t z = v % 4;
+    if (z=0)
+        printf("Yeah!");
+    else 
+        printf("End of program");
+return 0;
+}
+

4bb5256432c5acb5399315dd028b024e3b6dd352
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-08 14:20:14 +0200

    prints ASCII values of all characters

---
+++ week-01/day-05/ascii.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    for ( char i = 0; i < 127; i++)
+    printf("ASCII value of character %c = %d \n" , i, i);
+    
+    
+    return 0;
+}

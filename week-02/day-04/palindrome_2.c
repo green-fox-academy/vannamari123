0e67b2cffebb87ed28f42186b4bcaf31ecb9177c
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-12 17:19:19 +0200

    Finds palindrome in middle of string

--- week-02/day-04/palindrome_2.c
+++ week-02/day-04/palindrome_2.c
@@ -2,19 +2,27 @@
 //It should take a string, search for palindromes that at least 3 characters
 //long and return a list with the found palindromes.
 
-
 #include <stdio.h>
-#include <string>
+#include <string.h>
 
 int main()
 {
-  char str[100];
+    char str[100];
 
-  printf("Input a string\n");
-  gets(str);
+    printf("Input a string\n");
+    gets(str);
 
-  int n = strlen(str);
+    int n = strlen(str);
 
+    for (int i = 1; i < n; i++)
+    {
+        if(str[i-1] == str[i+1])
+        {
+            printf("%c%c%c\n", str[i-1], str[i], str[i+1]);
 
+        }
 
+    }
+
+    return 0;
 }

4b3a439717d6f9c9d9484ed1e0b13c70ab9426fd
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-12 16:03:35 +0200

    Asks for user string input

---
+++ week-02/day-04/palindrome_2.c
@@ -0,0 +1,20 @@
+//Create a function named search palindrome.
+//It should take a string, search for palindromes that at least 3 characters
+//long and return a list with the found palindromes.
+
+
+#include <stdio.h>
+#include <string>
+
+int main()
+{
+  char str[100];
+
+  printf("Input a string\n");
+  gets(str);
+
+  int n = strlen(str);
+
+
+
+}

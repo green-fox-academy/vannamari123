3a087911d0460015ce1875ed23b747f6330094ad
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-13 11:42:36 +0200

    Gives back random number

--- week-02/day-05/guee_my_number.c
+++ week-02/day-05/guee_my_number.c
@@ -1,49 +1,29 @@
 #include <stdio.h>
 
+int main()
+{
+    int r = 1 + rand() % 99;
+    int number = 0;
 
-/*
- Exercise
+    printf("I've the number between 1-100. Guess my number! You have 5 lives.\n");
 
- Write a program where the program chooses a number between 1 and 100. The player is then asked to enter a guess. If the player guesses wrong, then the program gives feedback and ask to enter an other guess until the guess is correct.
+    for (int i = 5 ; i > 0; i--)
+    {
+        scanf("%d", &number);
+        if (number == r)
+        {
+            printf("Congratulations! You won!\n");
+        } else if (number > r)
+        {
+            printf("Too high. You have %d lives left.\n", i);
+        } else
+        {
+            printf("Too low. You have %d lives left.\n", i);
+        }
+    }
+    printf("The number was:%d\n", r);
+    printf("You lost.\n");
 
- Make the range customizable (ask for it before starting the guessing).
- You can add lives. (optional)
- Example
+    return 0;
 
- I've the number between 1-100. You have 5 lives.
-
- 20
- Too high. You have 4 lives left.
-
- 10
- Too low. You have 3 lives left.
-
- 15
- Congratulations. You won!
-
- */
-
- int main()
- {
-     int r = 1 + rand() % 99;
-     int number = 0;
-
-     printf("I've the number between 1-100. Guess my number! You have 5 lives.\n");
-
-     for (int i = 5 ; i > 0; i--)
-     {
-         scanf("%d", &number);
-         if (number == r)
-         {
-             printf("Congratulations! You won!\n");
-         } else if (number > r)
-             printf("Too high. You have %d lives left.\n", i);
-         else
-             printf("Too low. You have %d lives left.\n", i);
-     }
-     printf("The number was:%d\n", r);
-     printf("You lost.\n");
-
-     return 0;
-
- }
+}

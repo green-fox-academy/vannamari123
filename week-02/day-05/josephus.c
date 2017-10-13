866113e0e1d9a3f918292ced72fdb099322dec05
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-13 13:38:15 +0200

    Creates variables

---
+++ week-02/day-05/josephus.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/*Write a function to solve Josephus Problem. The program should ask for a
+number, this number represents how many people are in the "game".
+The return value should be the number of the "winning" seat.
+*/
+
+
+
+
+
+int main()
+{
+  int nr = 0;
+  int count = 0;
+
+  printf("Type the number of people! \n");
+  scanf("%d", &nr);
+  
+
+
+
+    return 0;
+
+}

dd24c3cea87d9ce2bad38b837151e06619ca001a
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-17 15:39:17 +0200

    Solves error handling

---
+++ week-03/day-02/08.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//void transform (char str[], int original_base, int new_base);
+
+//TODO: write a program, which transforms a number from a number system to another.
+//use the stdlib.h functions, and take care of error handling (see the reference)
+//the transform() function should print out the result.
+
+int main()
+{
+    char str[20];
+    int original_base;
+    int new_base;
+
+    while (1)
+    {
+        printf("Give me a number to transform it in an other system: ");
+        scanf("%s", str);
+        printf("Give me the original base: ");
+        scanf("%d", &original_base);
+        if(original_base > 36 || original_base < 1)
+        {
+            printf("Base must be between 1 and 36.Try again!\n");
+            continue;
+        }
+        printf("Give me the number of the new base: ");
+        scanf("%d", &new_base);
+        if(new_base > 36 || new_base < 1)
+        {
+            printf("Base must be between 1 and 36.Try again!\n");
+            continue;
+        }
+        break;
+    }
+
+    transform(str, original_base, new_base);
+    return(0);
+}
+
+/*void transform (char* str, int original_base, int new_base)
+{
+
+
+
+}*/

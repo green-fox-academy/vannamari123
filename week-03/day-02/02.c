7d9d32202dac604720d43e22581551ba0ecb08ea
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-17 11:12:03 +0200

    Creates variables

---
+++ week-03/day-02/02.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+  //TODO: write a program wich adds up x and 1, and print out it rounded.
+
+
+int main()
+{
+	  double x = 1.2;
+    double  result = x + 1;
+
+    printf ("%.0f", result);
+
+    return 0;
+}

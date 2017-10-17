1a80de7ac0893b4409d5b61ff3f43713fc41bd50
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-17 11:04:47 +0200

    Prints casting result

---
+++ week-03/day-02/01.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+main()
+{
+    int  i = 2;
+    float f = 13.5, result;
+
+    //TODO: print out the f/i quotient
+
+		result = f / (float) i;
+		printf("%.3f\n", result );
+
+
+
+    //QUESTION: do you need explicit conversion?
+    // No. "result = i / f" works fine
+
+    return 0;
+}

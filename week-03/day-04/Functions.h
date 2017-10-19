ffa925454ccc4d6b143ecdd789d8df097f1506f1
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-19 17:58:13 +0200

    Styles heather file

--- week-03/day-04/Functions.h
+++ week-03/day-04/Functions.h
@@ -5,22 +5,11 @@
 
 
 void welcome_scr();
-
-
 float summation(float num1, float num2);
-
-
 float subtraction(float num1, float num2);
-
-
 float multiplication(float num1, float num2);
-
 float division(float num1, float num2);
-
 int div_rem(int num1, int num2);
-
 float squaring (float num1);
-
 double sq_root(double num1);
-
 float logarithm(float num1);

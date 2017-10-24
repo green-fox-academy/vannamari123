228cac8f24cecb51cb4bbd4711dc8718245f7fb8
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-24 12:55:37 +0200

    Prints memory address

---
+++ week-04/01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+//TODO:
+// print the memory address of the variable
+
+
+int main()
+{
+    int number = 1234;
+    printf("Memory address of the variable: %p\n", &number);
+
+    return 0;
+}

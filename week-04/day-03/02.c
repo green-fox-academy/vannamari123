af1175a79f095bcbe035081801e596fefb520839
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-25 16:04:49 +0200

    Creates separate funct to print

--- week-04/day-03/02.c
+++ week-04/day-03/02.c
@@ -4,6 +4,7 @@
 // Write a function that takes a Car as an argument and prints all it's stats
 // If the car is a Tesla it should not print it's gas level
 
+
 enum CarType {VOLVO, TOYOTA, LAND_ROVER, TESLA};
 
 struct Car {
@@ -12,18 +13,24 @@ struct Car {
     double gas;
 };
 
+void car_print(struct Car car_1);
 
 
 int main()
 {
+    struct Car car_test = {.type = TOYOTA, .km = 356, .gas = 45};
+    car_print(car_test);
 
-    struct Car car_1 = {.type = TESLA, .km = 356, .gas = 45};
+    return 0;
+}
 
+
+void car_print(struct Car car_1)
+{
     if (car_1.type != TESLA){
-    printf("Data of car no.%d:\nkm:%.0f, gas:%.0fl\n", car_1.type+1, car_1.km, car_1.gas);
+        printf("Data of car no.%d:\nkm:%.0f, gas:%.0fl\n", car_1.type+1, car_1.km, car_1.gas);
     } else {
         printf("Data of car no.%d:\nkm:%.0f\n", car_1.type+1, car_1.km);
     }
 
-    return 0;
 }

da6a9f3812a8a51014f0d786cccd3d5a142dd97d
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-26 13:27:03 +0200

    Worth buying fct modified

--- week-04/day-04/05.c
+++ week-04/day-04/05.c
@@ -1,50 +1,32 @@
-//05.c
-
-#include <stdio.h>
-#include <strings.h>
-
-// TODO:
-// Create a struct that represents a House
-// It should store:
-//  - The address of the house
-//  - The price in EUR
-//  - The number of rooms
-//  - The area of the house in square meters
-
-// TODO:
-// The market price of the houses is 400 EUR / square meters
-// Create a function that takes a pointer to a house and decides if it's worth to buy
-// (if the price is lower than the calculated price from it's area)
-
-// TODO:
-// Create a function that takes an array of houses (and it's length), and counts the
-// houses that are worth to buy.
-
 
 struct House {
     char address[100];
     int price;
-    int rooms;
     float area;
+    int rooms;
+
 };
 
-void buy(struct House *house);
+void worth_to_buy(struct House *house);
+
 
 int main() {
 
     struct House house_1;
 
     strcpy(house_1.address, "Sunny str 1");
-    house_1.price = 1000;
-    house_1.area = 2;
+    house_1.price = 40000;
+    house_1.area = 200;
+    house_1.rooms = 4;
 
-    buy(&house_1);
+
+    worth_to_buy(&house_1);
 
 
     return 0;
 }
 
-void buy(struct House *house)
+void worth_to_buy(struct House *house)
 {
     float sqrm_price = house->price / house->area;
 
@@ -52,4 +34,13 @@ void buy(struct House *house)
         printf("Worth buying.\n");
     else
         printf("Not worth buying.\n");
+}
+
+
+int nr_of_houses(int arr[])
+{
+    int number = 0;
+
+
+    return number;
 }

ccb1ab33ed29e37202a42781a8b1e66187c2e1b2
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-30 17:14:44 +0100

    Creates starting chess position

--- week-05/day-01/array_03.c
+++ week-05/day-01/array_03.c
@@ -16,7 +16,8 @@
 int main(){
     char arr[8][8];
 
-    for (int i = 0; i < 8; i++){
+//Creates empty chess table
+/*    for (int i = 0; i < 8; i++){
         for (int j = 0; j < 8; j++){
             if (i % 2 == 0 && j % 2 == 0){
                 arr[i][j] = 'X';
@@ -32,5 +33,31 @@ int main(){
 
         printf("\n");
     }
+*/
+
+//Creates starting position table
+
+for (int i = 0; i < 8; i++){
+    for (int j = 0; j < 8; j++){
+        if (i % 2 == 0 && j % 2 == 0){
+            arr[i][j] = 'X';
+        } else if (i % 2 != 0 && j % 2 !=0){
+            arr[i][j] = 'X';
+            arr[1][j] = 'P';
+        } else {
+            arr[i][j] = ' ';
+        }
+        arr[1][j] = arr[6][j] = 'P';
+        arr[0][0] = arr[7][7] = arr[0][7] = arr[7][0] = 'R'; //R stands for Rook
+        arr[0][1] = arr[0][6] = arr[7][1] = arr[7][6] = 'H'; //H stands for Horse
+        arr[0][2] = arr[0][5] = arr[7][2] = arr[7][5] = 'B'; //B stands for Bishop
+        arr[0][3] = arr[7][4] = 'Q'; //Q stands for Queen
+        arr[0][4] = arr[7][3] = 'K'; //K stands for King
+        printf("%c ", arr[i][j]);
+    }
+
+    printf("\n");
+}
+
     return 0;
 }

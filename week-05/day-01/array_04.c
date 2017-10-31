be42657eac287e8f7af5a33b021a0b0fc9827949
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-10-31 10:06:23 +0100

    Prints other sentence

--- week-05/day-01/array_04.c
+++ week-05/day-01/array_04.c
@@ -18,7 +18,7 @@
 
 int main()
 {
-    char arr_0[] = "The";
+/*    char arr_0[] = "The";
     char arr_1[] = "Quick";
     char arr_2[] = "Brown";
     char arr_3[] = "Dog";
@@ -43,6 +43,36 @@ int main()
     for (int i = 0; i < 9; i++){
     puts(ptr_arr[i]);
     }
+*/
+
+//Second part on the task
+
+  char arr_0[] = "The";
+  char arr_1[] = "Fox";
+  char arr_2[] = "is";
+  char arr_3[] = "green";
+  char arr_4[] = "and he";
+  char arr_5[] = "jumps";
+  char arr_6[] = "over";
+  char arr_7[] = "Lazy";
+  char arr_8[] = "Dog.";
+
+  char *ptr_arr[10];
+
+  ptr_arr[0] = arr_0;
+  ptr_arr[1] = arr_1;
+  ptr_arr[2] = arr_2;
+  ptr_arr[3] = arr_3;
+  ptr_arr[4] = arr_4;
+  ptr_arr[5] = arr_5;
+  ptr_arr[6] = arr_6;
+  ptr_arr[7] = arr_0;
+  ptr_arr[8] = arr_7;
+  ptr_arr[9] = arr_8;
+
+  for (int i = 0; i < 9; i++){
+      puts(ptr_arr[i]);
+  }
 
     return 0;
 }

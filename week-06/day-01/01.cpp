cfd37a8c25165d407a12e5542196a8e3db6607f0
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-11-13 12:54:05 +0100

    Greets classmates

---
+++ week-06/day-01/01.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+// Greet 3 of your classmates with this program, in three separate lines
+// like:
+//
+// Hello, Esther!
+// Hello, Mary!
+// Hello, Joe!
+// Store their names in separate string type variables
+
+
+int main()
+{
+    string a = "Laci";
+    string b = "Norbi";
+    string c = "Tamas";
+
+    cout << "Hello, " << a << "!" << endl;
+    cout << "Hello, " << b << "!" << endl;
+    cout << "Hello, " << c << "!" << endl;
+
+    return 0;
+}

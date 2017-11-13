fb6899db6be92a1639f374e5b2e65d0a5848f94f
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-11-13 12:49:21 +0100

    Prints out greeting

---
+++ week-06/day-01/00.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Modify this program to greet you instead of the World!
+
+int main()
+{
+    string a = "Annamari";
+    cout << "Hello " << a << "!" << endl;
+    return 0;
+}

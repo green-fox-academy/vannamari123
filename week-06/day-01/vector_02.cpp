3174f3b48b7e5e0d2e18f57d2af7538982b47193
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-11-13 15:25:15 +0100

    Creates vector with random int

---
+++ week-06/day-01/vector_02.cpp
@@ -0,0 +1,21 @@
+//Create a vector of integers
+//reserve place for 10 element, then fill those place with random numbers between 0 and 10
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+int main() {
+
+    vector<int> num_vector(10);
+
+    for (int i = 0; i < num_vector.size(); i++){
+        num_vector.at(i) = rand() % 20 + 1;
+        cout << num_vector[i] << " ";
+    }
+
+    return 0;
+}

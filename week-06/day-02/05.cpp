9055b868c3cc04c4679da941d1d740e6864ca8a6
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-11-14 11:46:07 +0100

    Creates mathod for circle are and circumference calculation

---
+++ week-06/day-02/05.cpp
@@ -0,0 +1,47 @@
+// Create a `Circle` class that takes it's radius as cinstructor parameter
+// It should have a `get_circumference` method that returns it's circumference
+// It should have a `get_area` method that returns it's area
+
+#include <iostream>
+#include <string>
+#include <cmath>
+
+using namespace std;
+
+class Circle{
+private:
+    unsigned int radius;
+
+public:
+    Circle(unsigned int radius){
+        this->radius = radius;
+    }
+
+    ~Circle(){
+        cout << "End of program" << endl;
+    }
+
+    int get_circumference()
+    {
+        unsigned int circumference = 2 * M_PI * radius;
+        return circumference;
+    }
+
+    int get_area()
+    {
+        unsigned int area = 2 * M_PI * pow(radius, 2);
+        return area;
+    }
+
+};
+
+
+
+int main() {
+
+    Circle c1(10);
+    cout << "Circumference: " << c1.get_circumference() << "units" << endl;
+    cout << "Area: " << c1.get_area() << "units^2" << endl;
+
+    return 0;
+}

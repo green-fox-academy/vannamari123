e507396b7138ba6eb3df409a8287561b5fc98b94
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-11-22 13:30:04 +0100

    Uses function overloading

--- week-07/day-03/04.cpp
+++ week-07/day-03/04.cpp
@@ -29,12 +29,33 @@ public:
         return this -> mNum += mNum;
     }
 
+    int add(int mNum, int num_one){
+        return this -> mNum += mNum + num_one;
+    }
+
+    int add(int mNum, int num_one, int num_two){
+        return this -> mNum += mNum + num_one + num_two;
+    }
+
+    int add(int mNum, int num_one, int num_two, int num_three){
+        return this -> mNum += mNum + num_one + num_two + num_three;
+    }
+
+    int add(int mNum, int num_one, int num_two, int num_three, int num_four){
+        return this -> mNum += mNum + num_one + num_two + num_three + num_four;
+    }
+
+
 
 };
 
 int main() {
-    Integer num1(20);
-    cout << num1.add(12) <<endl;
+    Integer number(10);
+    cout << number.add(1) << endl;
+    cout << number.add(1, 2) << endl;
+    cout << number.add(1, 2, 3) << endl;
+    cout << number.add(1, 2, 3, 4) << endl;
+    cout << number.add(1, 2, 3, 4, 5) << endl;
 
     return 0;
 }

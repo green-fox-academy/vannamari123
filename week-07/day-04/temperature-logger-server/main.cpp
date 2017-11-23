60d62def54861b2dba4a38cc9b33208a960bf41e
Annamaria Varga <varga.annamaria@yahoo.com>
2017-11-23 14:30:41 +0100

    Creates welcome screen and opens port

--- week-07/day-04/temperature-logger-server/main.cpp
+++ week-07/day-04/temperature-logger-server/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <stdio.h>
 #include <string>
 #include <vector>
+#include <cstdlib>
 
 #include "SerialPortWrapper.h"
 
@@ -17,14 +19,71 @@ int main()
     // connection
 
         SerialPortWrapper *serial = new SerialPortWrapper("COM3", 115200);
-        serial->openPort();
-        string line;
+
+    string line;
+    string input;
+
+    cout << endl << "Welcom to Temperature Logger Application!" << endl << endl << endl << "To display command list press: h" << endl << "To exit press: e" << endl << endl;
+    cin >> input;
+        if(input == "h"){
+            cout << "==============================" << endl << "Temperature Logger Application " << endl << "==============================" << endl;
+            cout << " Commands:" << endl << "h        Show command list" << endl;
+            cout << "o        Open port" << endl << "s        Start logging / Stop logging" << endl;
+            cout << "c        Close port" << endl << "l        List after error handling" << endl;
+            cout << "e        Exit from the program" << endl;
+        } else if (input == "e"){
+            exit(0);
+        } else {
+            cout << "Wrong command" << endl;
+        }
+
+    cin >> input;
+        while(input != "e"){
+            if (input == "o"){
+                serial->openPort();
+                while(1){
+                    serial->readLineFromPort(&line);
+                    if (line.length() > 0){
+                        cout << line << endl;
+                }
+            }
+        } break;
+        }
+
+
+/*
+
+if(input == "o"){
+            serial->openPort();
+            while(1){
+                serial->readLineFromPort(&line);
+                if (line.length() > 0){
+                    cout << line << endl;
+                }
+            }
+        }
+
+
+
+
+else if (input == "e"){
+        exit(0);}
+    else {
+    cout << "Wrong command" << endl;
+    }
+
+
+    serial->openPort();
+    string line;
         while(1){
-        serial->readLineFromPort(&line);
-        if (line.length() > 0){
-        cout << line << endl;
-        }
-        }
+            serial->readLineFromPort(&line);
+            if (line.length() > 0){
+                cout << line << endl;
+            }
+        }*/
         serial->closePort();
+
+
+
     return 0;
 }

09fa4d235cc8728ff8e78bb3a690bea78241766a
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2017-12-14 14:49:54 +0100

    Creates class & adds methods

---
+++ week-10/04.cpp
@@ -0,0 +1,55 @@
+//TASK NO. 4
+    //Create Animal class
+    //Every animal has a hunger value, which is a whole number
+    //Every animal has a thirst value, which is a whole number
+    //when creating a new animal object these values are created with the default 50 value
+    //Every animal can eat() which decreases their hunger by one
+    //Every animal can drink() which decreases their thirst by one
+    //Every animal can play() which increases both by one
+
+
+class Animal{
+private:
+    int hunger;
+    int thirst;
+public:
+    Animal(){
+        hunger = 50;
+        thirst = 50;
+    }
+
+    void eat(){
+        hunger--;
+    }
+
+    void drink(){
+        thirst--;
+    }
+
+    void play(){
+        hunger++;
+        thirst++;
+    }
+
+    void print(){
+        cout <<"Hunger value: " << hunger << endl << "Thirst value: " << thirst << endl;
+    }
+
+};
+
+
+int main(){
+
+    Animal zebra;
+    zebra.eat();
+    zebra.eat();
+    zebra.drink();
+    zebra.play();
+    zebra.play();
+
+    zebra.print();
+
+
+
+    return 0;
+}

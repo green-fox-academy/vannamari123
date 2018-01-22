d31dd44d92b991798d103c597499d787d0dd40eb
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2018-01-22 11:43:09 +0100

    Style fixed

--- week-01/day-03/08.c
+++ week-01/day-03/08.c
@@ -9,9 +9,9 @@ int main()
     int h2 = 200;
     int h3 = h2 * 2;
     if (h3 > h1) {
-        printf("A h3 nagyobb, mint h1.");
+        printf(" %d is larger than %d.", h3, h1);
     } else {
-        printf("A h3 kisebb, mint h1.");
+        printf("%d is smaller than %d.", h3, h1);
     }
     return 0;
 }

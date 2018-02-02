46b01f77e5695daa4f5af36443bd27342be18b50
vannamari123 <31822985+vannamari123@users.noreply.github.com>
2018-02-02 10:10:46 +0100

    Style fixed

--- FINAL_EXAM/exam_16.c
+++ FINAL_EXAM/exam_16.c
@@ -1,5 +1,6 @@
 //
-// TODO: Create a function that takes a string and two letters. The function should change every first letter to the second letter in the string.
+// TODO: Create a function that takes a string and two letters.
+//The function should change every first letter to the second letter in the string.
 //
 
 #include <stdio.h>
@@ -8,8 +9,8 @@
 
 void string_changer(char str[], char first_letter, char second_letter);
 
-int main(int argc, const char * argv[]) {
-
+int main(int argc, const char * argv[])
+{
     char my_string[] = "Hello world!";
     char my__other_string[] = "A long long long string";
 
@@ -23,11 +24,10 @@ int main(int argc, const char * argv[]) {
 void string_changer(char str[], char first_letter, char second_letter)
 {
     for (int i = 0; i < strlen(str); i++) {
-        if (str[i] == first_letter){
+        if (str[i] == first_letter) {
                 str[i] = second_letter;
         }
         printf("%c", str[i]);
     }
     printf("\n");
-
 }
